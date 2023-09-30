9af769345f4cd9e82446d636fbc1941edc7abf3c
Shakil57375 <shakil57375@gmail.com>
2023-09-30 11:16:30 +0600

    typescript arrays added

--- hello-world/dist/index.js
+++ hello-world/dist/index.js
@@ -1,3 +1,13 @@
 "use strict";
-let age = 20;
+let age = 21;
+let sales = 123343432;
+let course = "Shakil";
+let is_published = true;
+let label;
+function test(document) {
+    console.log(document);
+}
+let numbers = [3, 3];
+let number = [3, 5, 3];
+number.forEach(n => console.log(n.toString()));
 //# sourceMappingURL=index.js.map
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,3 +1,14 @@
-let age:number = 20;
-if(age<50)
-    age+=10;
+let age:number = 21;
+let sales = 123_343_432
+let course = "Shakil"
+let is_published = true;
+let label;
+function test(document:any){
+    console.log(document)
+}
+// arrays
+let numbers: number[] = [3, 3]// you can't write a string their because you have already declared it as  number of array.
+// another way
+let number = [3,5,3] // you have to tye the numbers only then it will automatically think that it's an array of number. If you type any string it will not give you error but it will lose the data-types.
+
+number.forEach(n => console.log(n.toString()))
