74ddcc8c95282206b1d99be47edda564b1fe3bac
Shakil57375 <shakil57375@gmail.com>
2023-10-02 17:47:53 +0600

    functions common errors added

--- Sumit_Saha_typescript/2_Editor_Setup_and_Compliation/src/script.ts
+++ Sumit_Saha_typescript/2_Editor_Setup_and_Compliation/src/script.ts
@@ -1,6 +1,6 @@
-const country = "Bangladesh"
-console.log(country)
-
+const country = "Bangladesh";
+console.log(country);
+// common errors when we work with the variables.
 /* let playerName = "Mash"
 console.log(playerName)
 // playerName = 32;// Assigning a number to a variable previously assigned a string in TypeScript results in a type error. The error will be "type 'number' is not assignable to type 'string'"
@@ -9,7 +9,22 @@ console.log(playerName) */
 // When declaring and defining a variable together, TypeScript can identify errors. If a variable is declared first and then assigned a string value, it can be reassigned a different datatype without any errors in TypeScript.
 // That's what happened here
 let playerName;
-console.log(playerName)// undefined
-playerName = "Tamim"
+console.log(playerName); // undefined
+playerName = "Tamim";
 playerName = 33;
-console.log(playerName)
+console.log(playerName);
+
+// common errors when working with function in typescript
+// When we fail to specify the data types of the parameters, we might pass inappropriate arguments. For example, we could send a string when we need a number, or provide a number when we require a string. Fortunately, in TypeScript, defining the data types of the parameters can prevent these errors and save time. By specifying the data types, we can identify errors beforehand, which minimizes errors in the browser and highlights them in code editors. Therefore, it is essential to specify the data types of the parameters to avoid such issues.
+// not a good habit or practice.
+/* function multiply(a, b){
+    return a * b
+}
+
+console.log(multiply("ff",4)) */
+// good habit or practice.
+function multiply(a: number, b: number) {
+  return a * b;
+}
+
+console.log(multiply(3, 4));
