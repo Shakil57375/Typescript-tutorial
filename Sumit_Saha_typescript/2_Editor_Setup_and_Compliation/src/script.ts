a906728f78f2777b340b3a271f32bbc8f1018aaf
Shakil57375 <shakil57375@gmail.com>
2023-10-02 17:33:15 +0600

    variables errors added

--- Sumit_Saha_typescript/2_Editor_Setup_and_Compliation/src/script.ts
+++ Sumit_Saha_typescript/2_Editor_Setup_and_Compliation/src/script.ts
@@ -1,2 +1,15 @@
 const country = "Bangladesh"
 console.log(country)
+
+/* let playerName = "Mash"
+console.log(playerName)
+// playerName = 32;// Assigning a number to a variable previously assigned a string in TypeScript results in a type error. The error will be "type 'number' is not assignable to type 'string'"
+console.log(playerName) */
+// another way to declare will not provide any error
+// When declaring and defining a variable together, TypeScript can identify errors. If a variable is declared first and then assigned a string value, it can be reassigned a different datatype without any errors in TypeScript.
+// That's what happened here
+let playerName;
+console.log(playerName)// undefined
+playerName = "Tamim"
+playerName = 33;
+console.log(playerName)
