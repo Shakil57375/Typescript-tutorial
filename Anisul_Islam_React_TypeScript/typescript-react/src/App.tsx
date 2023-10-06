3ed6a76813f9133ece8452f03f68a88b54d66041
Shakil57375 <shakil57375@gmail.com>
2023-10-06 15:54:00 +0600

    array data type added

--- Anisul_Islam_React_TypeScript/typescript-react/src/App.tsx
+++ Anisul_Islam_React_TypeScript/typescript-react/src/App.tsx
@@ -2,10 +2,17 @@ import "./App.css"
 import { User } from "./components/User";
 
 function App() {
+  const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    isEmployed: true,
+    language : ["Bangla", "English"]
+  };
   return (
     <div className="App">
       <h1>User Management</h1>
-      <User name = "Shakil Hossain" age = {21} isRegistered = {true}/>
+      <User name = "Shakil Hossain" age = {21} isRegistered  = {true} language = {["Bangla", "English"]} user = {person}/>
     </div>
   );
 }
--- Anisul_Islam_React_TypeScript/typescript-react/src/components/User.tsx
+++ Anisul_Islam_React_TypeScript/typescript-react/src/components/User.tsx
@@ -2,12 +2,21 @@ type UserProps = {
   name: string;
   age: number;
   isRegistered: boolean;
+  language: string[];
+  person: {
+    firstName: string;
+    lastName: string;
+    age: number;
+    isEmployed: boolean;
+    language: string[];
+  };
 };
-// datatype : string, number, boolean, void, null
+// ? datatype : string, number, boolean, void, null
+// ? user defined types => object, array, enum, union any, custom type
 // basic version
 // export const User = (props : UserProps) => {
 // Recommended version
-export const User = ({ name, age, isRegistered }: UserProps) => {
+export const User = ({ name, age, isRegistered, language, person }: UserProps) => {
   return (
     <div>
       {/* basic version */}
@@ -16,9 +25,13 @@ export const User = ({ name, age, isRegistered }: UserProps) => {
       {/* Recommended version */}
       <p>{name}</p>
       <p>{age}</p>
-      {
-        isRegistered ? <p>Registered user</p> : <p>Not Register user</p>
-      }
+      {isRegistered ? <p>Registered user</p> : <p>Not Register user</p>}
+      <p>Speaks:</p>
+      {language.map((lang, index) => {
+        return <span key={index}>{lang}</span>;
+      })}
+      {/* another person data by using object */}
+      
     </div>
   );
 };
