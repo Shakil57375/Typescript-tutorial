2b60536ba11fca371e8f02b8a2930a511b9364d1
Shakil57375 <shakil57375@gmail.com>
2023-10-06 10:47:36 +0600

    type reusebility added

--- React_TypeScript/react-typescript-demo/src/Components/PersonList.tsx
+++ React_TypeScript/react-typescript-demo/src/Components/PersonList.tsx
@@ -1,19 +1,19 @@
+import { Name } from "./PersonTypes";
 type PersonListProps = {
-    names: {
-        first : string,
-        last : string
-    }[]
-}
-const PersonList = (props : PersonListProps) => {
+  names: Name[];
+};
+const PersonList = (props: PersonListProps) => {
   return (
     <div>
-        {props.names.map(name => {
-            return (
-                <h2 key={name.first}>{name.first} {name.last}</h2>
-            )
-        })}
+      {props.names.map((name) => {
+        return (
+          <h2 key={name.first}>
+            {name.first} {name.last}
+          </h2>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default PersonList
+export default PersonList;
