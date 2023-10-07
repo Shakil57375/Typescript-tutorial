9efa3b7151f054ec0da91fe3266d1bff0ad4de32
Shakil57375 <shakil57375@gmail.com>
2023-10-07 12:48:15 +0600

    onClick event added

--- Anisul_Islam_React_TypeScript/typescript-react/src/components/NewUser.tsx
+++ Anisul_Islam_React_TypeScript/typescript-react/src/components/NewUser.tsx
@@ -1,18 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, ChangeEvent,MouseEvent } from "react";
 
 const NewUser = () => {
-    const [userName, setUserName] = useState("")
-    const handleUserName = (event : React.ChangeEvent<HTMLInputElement>) =>{
-        setUserName(event?.target.value)
-    }
+  const [userName, setUserName] = useState("");
+  const handleUserName = (event: ChangeEvent<HTMLInputElement>) => {
+    setUserName(event?.target.value);
+  };
+  const handleButtonClick = (event: MouseEvent<HTMLButtonElement>) => {
+    console.log(userName)
+  };
   return (
-    <div className='user'>
-        <p>Create new user</p>
-        <input type="text" onChange={handleUserName} />
-        <p style={{textAlign : "center"}}>{userName}</p>
+    <div className="user">
+      <p>Create new user</p>
+      <p>On Change event</p>
+      <input type="text" onChange={handleUserName} value={userName} />
+      <p style={{ textAlign: "center" }}>{userName}</p>
+      {/* onClick event */}
+      <button onClick={handleButtonClick}>Show Name</button>
     </div>
+  );
+};
 
-  )
-}
-
-export default NewUser
+export default NewUser;
